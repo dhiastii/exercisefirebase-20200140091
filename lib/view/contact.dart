7492fcedbd1115bc1a5613a94c38723d6f1c8d4c
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-06-10 23:39:28 +0700

    menambahkan login dan edit contact.dart

--- lib/view/contact.dart
+++ lib/view/contact.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:materi3pam/controller/contact_controller.dart';
+import 'package:materi3pam/view/add_contact.dart';
 import 'package:materi3pam/view/update_contact.dart';
 import 'package:flutter/material.dart';
 
@@ -46,64 +47,55 @@ class _ContactState extends State<Contact> {
                     itemCount: data.length,
                     itemBuilder: (context, index) {
                       return Padding(
-                          padding: const EdgeInsets.all(5.0),
-                          child: InkWell(
-                            onLongPress: () {},
-                            child: Card(
-                              elevation: 10,
-                              child: ListTile(
-                                leading: CircleAvatar(
-                                  backgroundColor: Colors.blue,
-                                  child: Text(
-                                    data[index]['name']
-                                        .substring(0, 1)
-                                        .toUpperCase(),
-                                    style: const TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold),
-                                  ),
+                        padding: const EdgeInsets.all(5.0),
+                        child: InkWell(
+                          onLongPress: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => UpdateContact(
+                                    id: data[index]['id'],
+                                    name: data[index]['name'],
+                                    phone: data[index]['phone'],
+                                    email: data[index]['email'],
+                                    address: data[index]['address']),
+                              ),
+                            );
+                          },
+                          child: Card(
+                            elevation: 10,
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                backgroundColor: Colors.purpleAccent,
+                                child: Text(
+                                  data[index]['name']
+                                      .substring(0, 1)
+                                      .toUpperCase(),
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                                title: Text(data[index]['name']),
-                                subtitle: Text(data[index]['phone']),
-                                trailing: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      // IconButton(
-                                      //   icon: const Icon(Icons.edit),
-                                      //   onPressed: () {
-                                      //     /// Call the update class or navigate to the update page
-                                      //     Navigator.push(
-                                      //         context,
-                                      //         MaterialPageRoute(
-                                      //           builder: (context) =>
-                                      //               UpdateContact(
-                                      //                   id: data[index]['id'],
-                                      //                   beforename: data[index]
-                                      //                       ['name'],
-                                      //                   beforephone: data[index]
-                                      //                       ['phone'],
-                                      //                   beforeemail: data[index]
-                                      //                       ['email'],
-                                      //                   beforeaddress:
-                                      //                       data[index]
-                                      //                           ['address']),
-                                      //         ));
-                                      //   },
-                                      // ),
-                                      IconButton(
-                                        icon: const Icon(Icons.delete),
-                                        onPressed: () {
-                                          cc.updateContact(
-                                              data[index]['id'].toString());
-                                          setState(() {
-                                            cc.getContact();
-                                          });
-                                        },
-                                      ),
-                                    ]),
+                              ),
+                              title: Text(data[index]['name']),
+                              subtitle: Text(data[index]['phone']),
+                              trailing: IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () {
+                                  cc.removeContact(
+                                      data[index]['id'].toString());
+                                  setState(() {
+                                    cc.getContact();
+                                  });
+
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                          content: Text('Contact Deleted')));
+                                },
                               ),
                             ),
-                          ));
+                          ),
+                        ),
+                      );
                     },
                   );
                 },
@@ -117,7 +109,7 @@ class _ContactState extends State<Contact> {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => UpdateContact(),
+              builder: (context) => const AddContact(),
             ),
           );
         },
---
+++ lib/view/login.dart
@@ -0,0 +1,126 @@
+import 'package:materi3pam/controller/auth_controller.dart';
+import 'package:materi3pam/model/user_model.dart';
+import 'package:materi3pam/view/contact.dart';
+import 'package:materi3pam/view/register.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  Login({super.key});
+  final formkey = GlobalKey<FormState>();
+
+  final authCr = AuthController();
+
+  @override
+  Widget build(BuildContext context) {
+    String? email;
+
+    String? password;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Form(
+            key: formkey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Email'),
+                  onChanged: (value) {
+                    email = value;
+                  },
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(hintText: 'Password'),
+                  onChanged: (value) {
+                    password = value;
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (formkey.currentState!.validate()) {
+                      UserModel? registeredUser = await authCr
+                          .signInWithEmailAndPassword(email!, password!);
+
+                      if (registeredUser != null) {
+                        // Registration successful
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('Login Successful'),
+                              content: const Text(
+                                  'You have been successfully login.'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () {
+                                    print(registeredUser.name);
+                                    Navigator.push(context,
+                                        MaterialPageRoute(builder: (context) {
+                                      return Contact();
+                                    }));
+                                    //Navigate to the next screen or perform any desired action
+                                  },
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      } else {
+                        // Registration failed
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('Login Failed'),
+                              content:
+                                  const Text('An error occurred during login.'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    }
+                  },
+                  child: const Text('Login'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Register(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
